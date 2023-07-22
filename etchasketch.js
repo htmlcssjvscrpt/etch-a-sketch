b2ff88ce4a0f404bc75f59784da339a54e6ca31d
Minh Frances <131257637+htmlcssjvscrpt@users.noreply.github.com>
2023-07-23 07:12:06 +0800

    Add etchasketch.js

---
+++ etchasketch.js
@@ -0,0 +1,16 @@
+const container = document.querySelector('#container');
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.style.display = 'flex';
+    for (let j = 0; j < 16; j++) {
+        const cell = document.createElement('div');
+        cell.style.width = '50px';
+        cell.style.height = '50px';
+        cell.style.border = '1px solid black';
+        cell.style.backgroundColor = 'pink';
+        row.appendChild(cell);
+    }
+    container.appendChild(row);
+}
+
+
