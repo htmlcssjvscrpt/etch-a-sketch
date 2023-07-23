93fdf6384ef3cf4b25ffa844af196dfe34e5eeb3
Minh Frances <131257637+htmlcssjvscrpt@users.noreply.github.com>
2023-07-23 09:08:34 +0800

    Add mouseover event

--- etchasketch.js
+++ etchasketch.js
@@ -1,18 +1,34 @@
 const container = document.querySelector('#container');
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.style.display = 'flex';
-    for (let j = 0; j < 16; j++) {
-        const cell = document.createElement('div');
-        cell.style.width = '50px';
-        cell.style.height = '50px';
-        cell.style.border = '1px solid black';
-        cell.style.backgroundColor = 'pink';
-        row.appendChild(cell);
+
+function createCrid(number) {
+    for (let i = 0; i < number; i++) {
+        const row = document.createElement('div');
+        row.style.display = 'flex';
+        for (let j = 0; j < number; j++) {
+            const cell = document.createElement('div');
+            cell.style.width = '50px';
+            cell.style.height = '50px';
+            cell.style.border = '1px solid black';
+            cell.style.backgroundColor = 'pink';
+
+            cell.setAttribute('data-cell', '');
+            row.appendChild(cell);
+         }
+        container.appendChild(row);
     }
-    container.appendChild(row);
 }
 
-addEventListener('mouseover', (e) =>{
-    e.target.style.backgroundColor = 'black';
+let parentContainer = document.querySelector('#container')
+parentContainer.addEventListener('mouseover', (e) => {
+    if (e.target.getAttribute('data-cell') !== null) {
+        e.target.style.backgroundColor = 'black';
+    }
 });
+
+let btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    let number = prompt('Enter the number of squares for the side the grid');
+    container.innerHTML='';
+    createCrid(number);
+});
+
